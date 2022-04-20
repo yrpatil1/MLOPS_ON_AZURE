4fb89c057df8f982452ab8b5a68aaec42ee58257
yrpatil1 <38290582+yrpatil1@users.noreply.github.com>
2022-04-20 13:11:30 +0530

    Update iris_training.py

--- iris_training.py
+++ iris_training.py
@@ -26,8 +26,8 @@ import joblib
 '''
 IRIS Classification
 '''
-__author__ = "Srijith.S"
-__email__ = "srssreejith@gmail.com"
+__author__ = "Yash"
+__email__ = "yash.patil@mediaagility.com"
 
 class IRISClassification():
     def __init__(self, args):
